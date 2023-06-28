9b41fbd0ad9c1c2d1eaffb3ec661f9bea46ff3e0
jozuko <jozuko.dev@gmail.com>
2023-06-28 12:32:09 +0900

    Done: Lesson2 layout

--- app/src/main/java/com/jozu/jetpack/compose/tutorial/MainActivity.kt
+++ app/src/main/java/com/jozu/jetpack/compose/tutorial/MainActivity.kt
@@ -3,13 +3,26 @@ package com.jozu.jetpack.compose.tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jozu.jetpack.compose.tutorial.model.Message
 import com.jozu.jetpack.compose.tutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +32,7 @@ class MainActivity : ComponentActivity() {
             ComposeTutorialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting(Message("Android", "Jetpack Compose"))
                 }
             }
         }
@@ -27,17 +40,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(message: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(
+                text = message.author,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = message.body,
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeTutorialTheme {
-        Greeting("Android")
+        Greeting(Message("Android", "Jetpack Compose"))
     }
 }
---
+++ app/src/main/java/com/jozu/jetpack/compose/tutorial/model/Message.kt
@@ -0,0 +1,3 @@
+package com.jozu.jetpack.compose.tutorial.model
+
+data class Message(val author: String, val body: String)
